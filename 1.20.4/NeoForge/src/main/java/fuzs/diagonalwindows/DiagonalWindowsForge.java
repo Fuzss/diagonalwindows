d619543bf9749f361fae2c642fa3c0cd9d4fb402
Fuzss <28218241+Fuzss@users.noreply.github.com>
2024-02-01 16:45:37 +0100

    prepare 1.20.4 port

---
+++ 1.20.4/NeoForge/src/main/java/fuzs/diagonalwindows/DiagonalWindowsForge.java
@@ -0,0 +1,16 @@
+package fuzs.diagonalwindows;
+
+import fuzs.puzzleslib.api.core.v1.ModConstructor;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLConstructModEvent;
+
+@Mod(DiagonalWindows.MOD_ID)
+@Mod.EventBusSubscriber(modid = DiagonalWindows.MOD_ID, bus = Mod.EventBusSubscriber.Bus.MOD)
+public class DiagonalWindowsForge {
+
+    @SubscribeEvent
+    public static void onConstructMod(final FMLConstructModEvent evt) {
+        ModConstructor.construct(DiagonalWindows.MOD_ID, DiagonalWindows::new);
+    }
+}
